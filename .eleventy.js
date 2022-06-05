b169067595c00d6a88b4ace6bfaa8218f162f1b9
Gary <kululu.gary@gmail.com>
2022-06-06 01:21:37 +0200

    added syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,15 @@
 const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/admin");
 
-  eleventyConfig.addFilter("postDate", (dateObj) => DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED));  
+  eleventyConfig.addFilter("postDate", (dateObj) => DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED));
   eleventyConfig.addNunjucksGlobal("getYear", () => new Date().getFullYear());
+
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
     dir: {
--- src/_data/repos.js
+++ src/_data/repos.js
@@ -19,15 +19,6 @@ function getDefaultData() {
 module.exports = async function () {
   if (process.env.NODE_ENV === "development") {
     return getDefaultData();
-    // const { defaultData } = dataJson;
-    // return defaultData
-    //   .sort((a, b) => b.stargazers_count - a.stargazers_count)
-    //   .slice(0, 3)
-    //   .map((repo) => {
-    //     const languages = dataJson[repo.full_name];
-
-    //     return { ...repo, language: Object.keys(languages)[0] };
-    //   });
   }
 
   const data = await axios
@@ -47,15 +38,4 @@ module.exports = async function () {
     .catch(getDefaultData);
 
   return data;
-
-  // const { data } = await axios.get(BASE_API + "users/KuluGary/repos");
-  // return data
-  //   .sort((a, b) => b.stargazers_count - a.stargazers_count)
-  //   .slice(0, 3)
-  //   .map(async (repo) => {
-  //     return;
-  //     // const { data: languages } = axios.get(BASE_API + "repos/KuluGary/" + repo.full_name);
-
-  //     // return { ...repo, language: Object.keys(languages)[0] };
-  //   });
 };
