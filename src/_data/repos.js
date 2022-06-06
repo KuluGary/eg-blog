8b1deee91a34bddacf621a96e580ba0900f5e219
Gary <kululu.gary@gmail.com>
2022-06-06 16:05:57 +0200

    fixed github repo fetching

--- src/_data/repos.js
+++ src/_data/repos.js
@@ -6,6 +6,7 @@ const BASE_API = "https://api.github.com/";
 
 function getDefaultData() {
   const { defaultData } = dataJson;
+
   return defaultData
     .sort((a, b) => b.stargazers_count - a.stargazers_count)
     .slice(0, 3)
@@ -21,26 +22,16 @@ module.exports = async function () {
     return getDefaultData();
   }
 
-  const data = await axios
-    .get(BASE_API + "users/KuluGary/repos")
-    .then((res) => {
-      const { data } = res;
-
-      return data
-        .sort((a, b) => b.stargazers_count - a.stargazers_count)
-        .slice(0, 3)
-        .map(async (repo) => {
-          const url = BASE_API + "repos/" + repo.full_name + "/languages";
-          const languages = await axios.get(url);
-
-          if (Object.keys(languages).length > 0) return { ...repo, language: Object.keys(languages)[0] };
-
-          return { ...repo, language: "" };
-        });
+  const response = await axios
+    .get(BASE_API + "users/KuluGary/repos", {
+      auth: {
+        username: process.env.GITHUB_ID,
+        password: process.env.GITHUB_SECRET,
+      },
     })
-    .catch((err) => {
-      getDefaultData();
-    });
+    .catch((err) => console.error(err));
 
-  return data;
+  if (response.data) return response.data.sort((a, b) => b.stargazers_count - a.stargazers_count).slice(0, 3);
+
+  return getDefaultData();
 };
